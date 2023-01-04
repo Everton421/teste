87db62b66725635561f9d794693edcd22a2ddc4b
Everton421 <71898208+Everton421@users.noreply.github.com>
2023-01-04 16:51:47 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2'
+import './styles/App.css'
+
+function App() {
+ 
+  return (
+  <div>
+  <Navbar/>
+
+  
+   </div>
+    )
+}
+
+export default App;
